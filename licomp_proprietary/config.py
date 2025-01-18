7810554244f7b648bb19adb4c44574ee023ec330
hesa <hesa@localhost>
2025-01-18 23:16:49 +0000

    Release 0.5.0

--- licomp_proprietary/config.py
+++ licomp_proprietary/config.py
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-licomp_proprietary_version = '0.4.1'
+licomp_proprietary_version = '0.5.0'
 my_supported_api_version = '0.5'
 
 repo = 'https://github.com/hesa/licomp-proprietary/'
